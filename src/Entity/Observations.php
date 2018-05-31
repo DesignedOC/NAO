19b8d1f86323b7763fb7f3bd4791766fa6f4c242
stephanie houssin <stephanie.houssin@gmx.fr>
2018-05-31 11:56:06 +0200

    install vich uploaderBundle

--- src/Entity/Observations.php
+++ src/Entity/Observations.php
@@ -40,7 +40,7 @@ class Observations
     private $longitude;
 
     /**
-     * @ORM\Column(type="string",nullable=true)
+     * @ORM\Column(type="string")
      * @Assert\Valid
      */
     private $picture;
