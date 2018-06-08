269a768c918eeafe62bd1f83f9d25292e8c62eba
Guillaume G <macadition@gmail.com>
2018-06-08 03:12:10 +0200

    Create sidebar Mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,17 @@ const bootstrap = require('bootstrap');
 
 const $ = require('jquery');
 
-// $('#myModal').modal('show');
+
+$(function () {
+
+    $('div.close i.material-icons, .overlay').on('click', function () {
+        $('#sidebar-wrapper').removeClass('active');
+        $('.overlay').fadeOut();
+    });
+
+    $('.navbar-toggler').on('click', function () {
+        $('#sidebar-wrapper').addClass('active');
+        $('.overlay').fadeIn();
+    });
+});
+
