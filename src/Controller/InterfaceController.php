913fb2d05681f4e3a977875f34aab3ac57c24d4a
stephanie houssin <stephanie.houssin@gmx.fr>
2018-06-10 14:04:41 +0200

    add path in interfaceController + add path nao_interface_memory in _sidebar.html.twig

--- src/Controller/InterfaceController.php
+++ src/Controller/InterfaceController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 class InterfaceController extends Controller
 {
     /**
-     * @Route("/interface/", name="interface")
+     * @Route("/interface/", name="nao_interface")
      */
     public function index()
     {
@@ -22,5 +22,40 @@ class InterfaceController extends Controller
     public function memory(Request $request)
     {
         return $this->render('interface/memory.html.twig');
+    }
+
+    /**
+     * @Route("/interface/devenir-naturaliste", name="nao_interface_naturalist")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function naturaliste(Request $request)
+    {
+        if($this->isGranted('ROLE_NATURALIST')){
+            return $this->redirectToRoute('nao_interface');
+        }
+        $user = $this->getUser();
+        $application = new Application();
+        $form = $this->createForm(NaturalistType::class, $application);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $application->setUser($user);
+            $this->getDoctrine()->getManager()->persist($application);
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Votre candidature est bien prise en compte. Veuillez patienter pour obtenir une réponse.');
+        }
+        return $this->render('interface/naturaliste.html.twig', [
+            'application' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/interface/carte", name="nao_interface_carte")
+     */
+    public function carte()
+    {
+        return $this->render('interface/carte.html.twig', [
+            'controller_name' => 'InterfaceController',
+        ]);
     }
 }
