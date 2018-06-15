083e98ca68b30790da8e7bf029552188bab9b392
stephanie houssin <stephanie.houssin@gmx.fr>
2018-06-15 15:29:30 +0200

    easy_admin becomes admin Route + add import and remove bdd in admin + add countNb method in BirdRepository + add condition for persist or remove base in Taxref Service

--- src/Controller/BirdController.php
+++ src/Controller/BirdController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class BirdController extends Controller
 {
     /**
--- src/Services/TaxrefBaseImport.php
+++ src/Services/TaxrefBaseImport.php
@@ -20,45 +20,66 @@ class TaxrefBaseImport
         $csvEncodage = mb_convert_encoding($csvContents, "UTF-8","Windows-1252");
         $datas = $serializer->decode($csvEncodage, 'csv');
 
+        $birdRepository = $this->em->getRepository('App:Bird');
+        $nb = $birdRepository->countNb();
+
+        if(empty($nb))
+        {
             foreach($datas as $data)
             {
-                $Bird = new Bird();
-                $Bird->setRegne($data['REGNE']);
-                $Bird->setPhylum($data['PHYLUM']);
-                $Bird->setClasse($data['CLASSE']);
-                $Bird->setOrdre($data['ORDRE']);
-                $Bird->setFamille($data['FAMILLE']);
-                $Bird->setCdNom($data['CD_NOM']);
-                $Bird->setCdTaxsup($data['CD_TAXSUP']);
-                $Bird->setCdRef($data['CD_REF']);
-                $Bird->setRang($data['RANG']);
-                $Bird->setLbNom($data['LB_NOM']);
-                $Bird->setLbAuteur($data['LB_AUTEUR']);
-                $Bird->setNomComplet($data['NOM_COMPLET']);
-                $Bird->setNomValide($data['NOM_VALIDE']);
-                $Bird->setNomVern($data['NOM_VERN']);
-                $Bird->setNomVernEng($data['NOM_VERN_ENG']);
-                $Bird->setHabitat($data['HABITAT']);
-                $Bird->setStatutFR($data['FR']);
-                $Bird->setStatutGF($data['GF']);
-                $Bird->setStatutMAR($data['MAR']);
-                $Bird->setStatutGUA($data['GUA']);
-                $Bird->setStatutSM($data['SM']);
-                $Bird->setStatutSB($data['SB']);
-                $Bird->setStatutSPM($data['SPM']);
-                $Bird->setStatutMAY($data['MAY']);
-                $Bird->setStatutEPA($data['EPA']);
-                $Bird->setStatutREU($data['REU']);
-                $Bird->setStatutSA($data['SA']);
-                $Bird->setStatutTA($data['TA']);
-                $Bird->setStatutNC($data['NC']);
-                $Bird->setStatutWF($data['WF']);
-                $Bird->setStatutPF($data['PF']);
-                $Bird->setStatutCLI($data['CLI']);
-                $this->em->persist($Bird);
+                $bird = new Bird();
+                $bird->setRegne($data['REGNE']);
+                $bird->setPhylum($data['PHYLUM']);
+                $bird->setClasse($data['CLASSE']);
+                $bird->setOrdre($data['ORDRE']);
+                $bird->setFamille($data['FAMILLE']);
+                $bird->setCdNom($data['CD_NOM']);
+                $bird->setCdTaxsup($data['CD_TAXSUP']);
+                $bird->setCdRef($data['CD_REF']);
+                $bird->setRang($data['RANG']);
+                $bird->setLbNom($data['LB_NOM']);
+                $bird->setLbAuteur($data['LB_AUTEUR']);
+                $bird->setNomComplet($data['NOM_COMPLET']);
+                $bird->setNomValide($data['NOM_VALIDE']);
+                $bird->setNomVern($data['NOM_VERN']);
+                $bird->setNomVernEng($data['NOM_VERN_ENG']);
+                $bird->setHabitat($data['HABITAT']);
+                $bird->setStatutFR($data['FR']);
+                $bird->setStatutGF($data['GF']);
+                $bird->setStatutMAR($data['MAR']);
+                $bird->setStatutGUA($data['GUA']);
+                $bird->setStatutSM($data['SM']);
+                $bird->setStatutSB($data['SB']);
+                $bird->setStatutSPM($data['SPM']);
+                $bird->setStatutMAY($data['MAY']);
+                $bird->setStatutEPA($data['EPA']);
+                $bird->setStatutREU($data['REU']);
+                $bird->setStatutSA($data['SA']);
+                $bird->setStatutTA($data['TA']);
+                $bird->setStatutNC($data['NC']);
+                $bird->setStatutWF($data['WF']);
+                $bird->setStatutPF($data['PF']);
+                $bird->setStatutCLI($data['CLI']);
+                $this->em->persist($bird);
             }
             $this->em->flush();
-        return " Le fichier a été correctement importé en Base de données";
+            return " Le fichier a été correctement importé en Base de données";
+        }
+
+        elseif(!empty($nb))
+        {
+            $birds = $this->em->getRepository('App:Bird')->findAll();
+            foreach($birds as $bird)
+            {
+                $this->em->remove($bird);
+            }
+            $this->em->flush();
+
+
+            }
+        return " Le fichier a été correctement supprimé de la base de données";
+
+
         }
 
 
