d99b1f07e18af859a66be584377acee7de50f939
juliettelofaro <juliette.lofaro@gmail.com>
2018-06-20 02:44:00 +0200

    add 2 new function to find the bird

--- src/Migrations/Version20180620003101.php
+++ src/Migrations/Version20180620003101.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180617161610 extends AbstractMigration
+final class Version20180620003101 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
