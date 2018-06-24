45f13c96810272256c2cea5137dc13c209b53fc8
juliettelofaro <juliette.lofaro@gmail.com>
2018-06-24 18:12:00 +0200

    make the form add an observation

--- src/Controller/BirdController.php
+++ src/Controller/BirdController.php
@@ -2,51 +2,23 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Services\TaxrefBaseImport;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Bird;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BirdController extends Controller
 {
-    protected $em;
-
     /**
-     * BirdController constructor.
-     * @param EntityManagerInterface $em
+     * @Route("/bird", name="bird")
      */
-    public function __construct(EntityManagerInterface $em)
+    public function index()
     {
-        $this->em = $em;
+        return $this->render('bird/index.html.twig', [
+            'controller_name' => 'BirdController',
+        ]);
     }
-
-    /**
-     * @Route ("admin/taxrefBaseImport", name="taxrefBaseImport")
-     * @param Request $request
-     * @param TaxrefBaseImport $taxref
-     * @return Response
-     */
-    public function taxrefBaseImport(Request $request, TaxrefBaseImport $taxref)
-    {
-        $taxref->reloadTaxref();
-        $this->addFlash('success','import ok');
-        return $this->redirectToRoute('admin',['entity'=>'Bird', 'action'=> 'list']);
-    }
-
-
-//    /**
-//     * @Route("/bird", name="bird")
-//     */
-//    public function index()
-//    {
-//        return $this->render('bird/observations.html.twig', [
-//            'controller_name' => 'BirdController',
-//        ]);
-//    }
 
     /**
      * Autocomplete pour le lbnom
@@ -88,4 +60,3 @@ class BirdController extends Controller
         return new JsonResponse($nomVerns);
     }
 }
-
--- src/Repository/BirdRepository.php
+++ src/Repository/BirdRepository.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Repository;
-
 use App\Entity\Bird;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -10,27 +9,12 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method Bird[]    findAll()
  * @method Bird[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class BirdRepository extends  ServiceEntityRepository
+class BirdRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Bird::class);
     }
-
-    /**
-     * @return int|mixed
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function countNb()
-    {
-        $nb = $this
-            ->createQueryBuilder('b')
-            ->select('count(b) as nb')
-            ->getQuery()
-            ->getSingleScalarResult();
-       return $nb;
-    }
-
 //    /**
 //     * @return Bird[] Returns an array of Bird objects
 //     */
@@ -47,26 +31,18 @@ class BirdRepository extends  ServiceEntityRepository
         ;
     }
     */
-
-    /**
-     * Get the number of Birds
-     * @return mixed
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function findAllBirdsCount()
+    /*
+    public function findOneBySomeField($value): ?Bird
     {
-        $qb = $this->createQueryBuilder('u')
-            ->select('COUNT(u)')
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
         ;
-
-        return $qb->getQuery()->getSingleScalarResult();
     }
+    */
 
-
-    /**
-     * @param $term
-     * @return mixed
-     */
     public function findBirdByLbNom($term)
     {
         $qb = $this->createQueryBuilder('b');
@@ -77,10 +53,6 @@ class BirdRepository extends  ServiceEntityRepository
         return $qb->getQuery()->getResult();
     }
 
-    /**
-     * @param $term
-     * @return mixed
-     */
     public function findBirdByNomVern($term)
     {
         $qb = $this->createQueryBuilder('b');
