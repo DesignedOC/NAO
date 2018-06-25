1ff5c8a8fdf3949d86829fac1ef22a9e4f7591e9
juliettelofaro <juliette.lofaro@gmail.com>
2018-06-25 15:18:56 +0200

    test for seach an adress

--- src/Controller/ObservationController.php
+++ src/Controller/ObservationController.php
@@ -146,6 +146,23 @@ class ObservationController extends Controller
     public function carteAction(Request $request)
     {
     }
+
+        /**
+     * @Route("observation/ajouterr", name="ajouterr")
+     * @Template("interface/observation/ajouterr.html.twig")
+     * @param Request $request
+     */
+    public function ajouterrAction(Request $request)
+    {
+    }
+        /**
+     * @Route("observation/test", name="test")
+     * @Template("interface/observation/test.html.twig")
+     * @param Request $request
+     */
+    public function testAction(Request $request)
+    {
+    }
     /**
      * @Route("map-search", name="map_search")
      * @param Request $request
